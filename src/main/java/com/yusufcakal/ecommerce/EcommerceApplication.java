587d6ee7de71331792359418d727d71742e9cfe2
yusufcakal <info@yusufcakal.com>
2018-03-25 03:23:29 +0300

    configuration commit

--- src/main/java/com/yusufcakal/ecommerce/EcommerceApplication.java
+++ src/main/java/com/yusufcakal/ecommerce/EcommerceApplication.java
@@ -3,12 +3,15 @@ package com.yusufcakal.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+
 }
